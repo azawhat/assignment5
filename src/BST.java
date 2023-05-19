2d3abb89c70b63aecb278bdd4c9bd96a43db9db1
azawhat <azamat.erezhepbayev@gmail.com>
2023-05-20 02:08:01 +0600

    copying variables from the code in the moodle

--- src/BST.java
+++ src/BST.java
@@ -1,6 +1,8 @@
 public class BST<K extends Comparable<K>,V> {
     private Node root;
     private class Node{
-
+        private K key;
+        private V value;
+        private Node left, right;
     }
 }
