29427e3de2dd3bd2dd097d161ecbff482c15dfd1
azawhat <azamat.erezhepbayev@gmail.com>
2023-05-21 01:28:13 +0600

    defining `currentIndex` variable

--- src/BST.java
+++ src/BST.java
@@ -104,6 +104,8 @@ public class BST<K extends Comparable<K>,V> implements Iterable<BST.Entry<K, V>>
 
     public class BSTIterator implements Iterator<Entry<K, V>> {
         private List<Entry<K, V>> entries;
+        private int currentIndex;
+
 
 
 
