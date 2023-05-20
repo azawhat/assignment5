acc8cd8025108b5c9b14d7621ccf1467243a8f28
azawhat <azamat.erezhepbayev@gmail.com>
2023-05-20 22:39:51 +0600

    implementing `put` method

--- src/BST.java
+++ src/BST.java
@@ -11,8 +11,28 @@ public class BST<K extends Comparable<K>,V> {
             this.value = value;
         }
     }
-    public void put(K key, V value){}
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, value);
+        else if (cmp > 0)
+            node.right = put(node.right, key, value);
+        else
+            node.value = value;
+        return node;
+    }
+
     public V get(K key) {}
     public void delete(K key) {}
     public Iterable<K> iterator(){}
+
+
 }
